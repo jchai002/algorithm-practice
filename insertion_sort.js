a5f9fbfa7548169d049ff9ebb4dc4fe3ac2fb416
Jerry Chai <jchai002@gmail.com>
2015-12-08 14:43:31 -0800

    insertion sort edit

--- insertion_sort.js
+++ insertion_sort.js
@@ -6,6 +6,12 @@ var insert = function(array, rightIndex, value) {
     array[i + 1] = value;
 };
 
+var insertionSort = function(array) {
+    for(var i=1;i<array.length;i++){
+       insert(array,i-1,array[i]);
+    }
+};
+
 
 
 console.log(insertion_sort([1,3,2,5,4,7,6,-1,9,8]))
