2b345ded509038f4ffcda61dc967d03cb13ca5fd
Jerry Chai <jchai002@gmail.com>
2015-12-23 07:29:23 -1000

    add recursive palindrome

---
+++ palindrome.js
@@ -0,0 +1,34 @@
+// Returns the first character of the string str
+var firstCharacter = function(str) {
+    return str.slice(0, 1);
+};
+
+// Returns the last character of a string str
+var lastCharacter = function(str) {
+    return str.slice(-1);
+};
+
+// Returns the string that results from removing the first
+//  and last characters from str
+var middleCharacters = function(str) {
+    return str.slice(1, -1);
+};
+
+var isPalindrome = function(str) {
+    // base case #1
+    if (str.length<2) {
+        return true;
+    }
+    // base case #2
+    else if (firstCharacter(str)!==lastCharacter(str)){
+        return false;
+    } else {
+        return isPalindrome(middleCharacters(str));
+    }
+    // recursive case
+};
+
+var checkPalindrome = function(str) {
+    println("Is this word a palindrome? " + str);
+    println(isPalindrome(str));
+};
