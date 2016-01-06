d9a204b455c418a390e768a7d8e496a2a51b7fb9
Jerry Chai <jchai002@gmail.com>
2016-01-06 14:30:48 -0800

    add not in place quicksort

---
+++ quick_sort_2.js
@@ -0,0 +1,17 @@
+function quickSort(arr){
+  if (arr.length<2){
+    return arr
+  } else {
+    var pivot=arr.pop()
+    var bigger=[]
+    var smaller=[]
+    for (var i=0;i<arr.length;i++){
+      if(arr[i]<pivot){
+        smaller.push(arr[i])
+      } else {
+        bigger.push(arr[i])
+      }
+    }
+    return quickSort(smaller).concat(pivot, quickSort(bigger))
+  }
+}
