f3aee6822af141e0be339b058a7837acca13c876
Jerry Chai <jchai002@gmail.com>
2016-01-07 03:11:52 -0800

    clean up console logs

--- Outco/binary_search_tree.js
+++ Outco/binary_search_tree.js
@@ -57,17 +57,3 @@ BinarySearchTree.prototype.search=function(val){
   return traverse(this.root)
 }
 
-//testing cases
-
-var tree=new BinarySearchTree();
-tree.insert(-1)
-tree.insert(4)
-tree.insert(3)
-tree.insert(6)
-tree.insert(5)
-tree.insert(8)
-tree.insert(9)
-tree.insert(10)
-tree.insert(100)
-console.log(tree.search(8))
-console.log(tree.search(1000))
