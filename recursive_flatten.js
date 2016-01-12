7ae28f7c5711a76515189d05f04153be9f12ab02
Jerry Chai <jchai002@gmail.com>
2016-01-12 00:51:32 -0800

    solve recursive flatten

---
+++ recursive_flatten.js
@@ -0,0 +1,20 @@
+
+
+function flatten(array) {
+  var result=[]
+
+  function traverse(array){
+    for (var i=0;i<array.length;i++){
+      if (typeof array[i]==="number") {
+        result.push(array[i])
+      } else {
+          traverse(array[i])
+      }
+    }
+  }
+
+  traverse(array)
+  return result
+}
+
+console.log(flatten([[1,9],[3,4],[5,[6,[7]]]]))
